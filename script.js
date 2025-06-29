a5feb71e54f5818301dd6a998e43d36eae751c67
RaiN958 <andrianov.kostia2013@gmail.com>
2025-06-29 23:57:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,19 +18,15 @@ function dragElement(terrariumElement) {
         pos2 = 0,
         pos3 = 0,
         pos4 = 0;
-    terrariumElement.onpointerdown = pointerDrag;
-}
-
-function pointerDrag(e) {
+	terrariumElement.onpointerdown = pointerDrag;
+	function pointerDrag(e) {
     e.preventDefault();
     console.log(e);
     pos3 = e.clientX;
     pos4 = e.clientY;
 	document.onpointermove = elementDrag;
 	document.onpointerup = stopElementDrag;
-}
-
-function elementDrag(e) {
+		function elementDrag(e) {
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
@@ -39,8 +35,10 @@ function elementDrag(e) {
     terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
     terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
 }
-
 function stopElementDrag() {
     document.onpointerup = null;
     document.onpointermove = null;
 }
+}
+}
+
